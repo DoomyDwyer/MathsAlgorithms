022893a3ab60e8a84d00cc987b6b3bcd27cd621e
Steve <doomy@puscii.nl>
2018-11-07 00:15:44 +0000

    Add project files.

---
+++ Erastothenes/Erastothenes.cpp
@@ -0,0 +1,12 @@
+﻿// Erastothenes.cpp : Defines the entry point for the application.
+//
+
+#include "Erastothenes.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
