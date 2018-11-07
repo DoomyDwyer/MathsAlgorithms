8f774b70821f28efb0873be3622af378e7920608
Steve <doomy@puscii.nl>
2018-11-07 00:26:25 +0000

    Added TODO to actually implement algorithm (boiler plate code in place only)

--- Erastothenes/Erastothenes.cpp
+++ Erastothenes/Erastothenes.cpp
@@ -2,11 +2,32 @@
 //
 
 #include "Erastothenes.h"
+#include <vector>
 
 using namespace std;
 
+/*
+ *
+ * Input a number and use the Sieve of Erastothenes to display all prime numbers
+ * between 2 and the entered number
+ *
+*/
 int main()
 {
-	cout << "Hello CMake." << endl;
+	vector<bool>::size_type number;
+	cout << "Sieve of Erastosthenes" << endl;
+	cout << "Calculate primes between 2 and ";
+	cin >> number;
+	vector<bool> numbers(number + 1, true);
+
+	//	cout << "Primes <= " + number << endl;
+	for (decltype(numbers.size()) prime = 2; prime <= number; ++prime) {
+		// TODO: Implement algorithm
+		if (numbers[prime]) {
+			cout << prime << endl;
+		}
+	}
+	cout << endl << "Done." << endl;
+
 	return 0;
 }
