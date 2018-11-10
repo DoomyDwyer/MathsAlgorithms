2099c22a07fa00bdfd5884348b1c92d4b53fe504
Steve <doomy@puscii.nl>
2018-11-10 23:41:58 +0000

    Algorithm completed. First sqrt(N) integers are iterated through and their multiples 'crossed out'. All subsequent integers between sqrt(N) + 1 are by definition primes, so they are just displayed. I expect that the logarithmic running time of this algorithm will be advesely affected by the fact that I'm iterating through the rest of the collection and displaying all primes.

--- Erastothenes/Erastothenes.cpp
+++ Erastothenes/Erastothenes.cpp
@@ -1,8 +1,11 @@
-﻿// Erastothenes.cpp : Defines the entry point for the application.
+﻿// Erastothenes.cpp : The Sieve of Erastothenes .
 //
 
-#include "Erastothenes.h"
+#pragma once
+
+#include <iostream>
 #include <vector>
+#include <ctime>
 
 using namespace std;
 
@@ -14,20 +17,53 @@ using namespace std;
 */
 int main()
 {
-	vector<bool>::size_type number;
-	cout << "Sieve of Erastosthenes" << endl;
+	cout << "Sieve of Erastothenes" << endl;
 	cout << "Calculate primes between 2 and ";
-	cin >> number;
-	vector<bool> numbers(number + 1, true);
 
-	//	cout << "Primes <= " + number << endl;
-	for (decltype(numbers.size()) prime = 2; prime <= number; ++prime) {
-		// TODO: Implement algorithm
+	vector<bool>::size_type number;
+	cin >> number;
+
+	// Initialise N+1 bools in a vector:
+	// We'll ignore elements 0 & 1, allowing us to index the vector using the actual numbers
+	// we're working with (2..N)
+	// bools are set to true initially as they're all potential primes until 'crossed out' (i.e. set to false)
+	const vector<bool>::size_type LIST_LENGTH = number + 1;
+	vector<bool> numbers(LIST_LENGTH, true);
+
+	const decltype(numbers.size()) FIRST_PRIME = 2;
+	const decltype(numbers.size()) SQRT_OF_N = sqrt(number);
+	decltype(numbers.size()) count = 0;
+
+	// Get performance stats
+	const clock_t begin_time = clock();
+
+	decltype(numbers.size()) prime = FIRST_PRIME;
+	while (prime <= SQRT_OF_N) {
+		if (numbers[prime]) {
+			// Cross out all multiples of the prime numberas
+			for (decltype(numbers.size()) multiple = prime * 2; multiple < LIST_LENGTH; multiple += prime) {
+				if (numbers[multiple]) {
+					numbers[multiple] = false;
+				}
+			}
+
+			cout << prime << endl;
+			++count;
+		}
+		++prime;
+	}
+
+	// All subsequent 'uncrossed-out' numbers are, by definition, primes, so print them
+	while (prime < LIST_LENGTH) {
 		if (numbers[prime]) {
 			cout << prime << endl;
+			++count;
 		}
+		++prime;
 	}
-	cout << endl << "Done." << endl;
+
+	cout << endl << "There are " << count << " prime numbers between " << FIRST_PRIME << " and " << number << " ." << endl;
+	std::cout << "Time taken: " << float(clock() - begin_time) << " msecs";
 
 	return 0;
 }
